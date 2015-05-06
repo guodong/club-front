7bfff26d87b174b389a51ef192c6de49022d3a77
guodong <gd@tongjo.com>
2015-05-06 15:38:12 +0800

    demo

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var API = 'http://api.club.com/';
+var API = 'http://api.club.olege.com/';
 var club = angular.module("club", ['ngRoute'], function($interpolateProvider){
 	$interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
@@ -7,9 +7,26 @@ club.config(function($routeProvider) {
 	$routeProvider.when('/', {
 		controller: 'MainCtrl',
 		templateUrl: 'tpl/main.html',
+	}).when('/login', {
+		controller: 'LoginCtrl',
+		templateUrl: 'tpl/login.html',
 	}).otherwise({
 		redirectTo: '/'
 	});
 }).controller('MainCtrl', function($scope, $route){
 	
+}).controller('LoginCtrl', function($scope, $route, $http){
+	$scope.login = function(){
+		$http({
+			method: 'GET',
+			url: API+'accesstoken',
+			params: $scope.user
+		}).success(function(d){
+			if(!d.code){
+				location.href="#/";
+			}else{
+				alert(d.msg);
+			}
+		});
+	}
 });
